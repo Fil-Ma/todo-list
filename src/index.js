c809ec1a68a5951a381a2bda0a884e34b541643b
Fil-Ma <filippo.mazzalai@gmail.com>
2023-06-24 12:13:53 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 
 import App from "./app/App";
 import store from "./app/store";
-
+//test
 const rootElement = createRoot(document.getElementById("root"));
 
 rootElement.render(
